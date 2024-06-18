eca1d93d3385d1acd5d8f34fc5b0e926d5f9de40
ivan <i.razgonyaev@estesis.tech>
2024-06-18 16:14:08 +0700

    feature(event_file_type): Изменил политику для s3 бакета файлов мероприятий.

--- src/config/settings/s3/settings.py
+++ src/config/settings/s3/settings.py
@@ -1,4 +1,5 @@
-from pydantic import Field
+from pydantic import Field, field_validator
+from pydantic_core.core_schema import ValidationInfo
 
 from ..core import EnvCoreSettings
 
@@ -14,10 +15,35 @@ class S3Settings(EnvCoreSettings):
         "Statement": [
             {
                 "Effect": "Deny",
-                "Principal": {"AWS": "*"},
+                "Principal": "*",
+                "Action": ["s3:*"],
+                "Resource": ["arn:aws:s3:::{}", "arn:aws:s3:::{}/*"],
+            },
+            {
+                "Effect": "Allow",
+                "Principal": "*",
                 "Action": ["s3:GetObject"],
-                "Resource": "arn:aws:s3:::events/*",
+                "Resource": ["arn:aws:s3:::{}/*"],
+                "Condition": {
+                    "StringEquals": {"s3:authType": "REST-QUERY-STRING"}
+                },
             },
         ],
     }
     MAX_EVENTS_BUCKET_FILE_MB_SIZE: int = 10
+
+    @field_validator("EVENTS_BUCKET_POLICY", mode="before")
+    def assemble_events_bucket_policy(
+        cls, v: dict, values: ValidationInfo
+    ) -> dict:
+        events_bucket = values.data.get("EVENTS_BUCKET", "events")
+        v["Statement"][0]["Resource"][0] = v["Statement"][0]["Resource"][
+            0
+        ].format(events_bucket)
+        v["Statement"][0]["Resource"][1] = v["Statement"][0]["Resource"][
+            1
+        ].format(events_bucket)
+        v["Statement"][1]["Resource"][0] = v["Statement"][1]["Resource"][
+            0
+        ].format(events_bucket)
+        return v
