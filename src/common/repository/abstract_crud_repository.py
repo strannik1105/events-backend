22a0028c631343b29f7884891a98e1b63cf51663
Daniil325 <potekhindaniill@mail.ru>
2024-09-01 22:46:40 +0700

    feat: addition of crud service functions

--- src/common/repository/abstract_crud_repository.py
+++ src/common/repository/abstract_crud_repository.py
@@ -6,21 +6,21 @@ T = TypeVar("T")
 
 class AbstractCrudRepository(Generic[T], Protocol):
     # returns all obj instanses
-    def get_all(limit: int = 100, offset: int = 0) -> list[T]:
+    def get_all(self, limit: int = 100, offset: int = 0) -> list[T]:
         pass
 
     # return only one obj instance with provided sid or None
-    def get(sid: UUID) -> T | None:
+    def get(self, sid: UUID) -> T | None:
         pass
 
     # return created obj or None
-    def create(obj: T) -> T | None:
+    def create(self, obj: T) -> T | None:
         pass
 
     # update obj instance
-    def update(obj: T, changes, sid):
+    def update(self, obj: T, changes, sid):
         pass
 
     # delete obj
-    def delete(obj: T):
+    def delete(self, obj: T):
         pass
--- src/common/services/crud_service.py
+++ src/common/services/crud_service.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from common.db.base import SQLAlchemyBaseModel
 from common.repository.abstract_crud_repository import AbstractCrudRepository
 from common.services.abstract_crud_service import AbstractCrudService
@@ -8,8 +10,17 @@ class CrudService(AbstractCrudService):
         self._repository = repository
 
     async def get_all(self, limit, offset) -> list[SQLAlchemyBaseModel]:
-        print(await self._repository.get_all(limit, offset))
         return await self._repository.get_all(limit, offset)
+
+    async def get_one(self, sid: UUID):
+        return await self._repository.get(sid)
 
     async def create(self, obj):
         await self._repository.create(obj)
+
+    async def update(self, obj, changes, sid):
+        return await self._repository.update(obj, changes, sid)
+
+    async def delete(self, obj):
+        await self._repository.delete(obj)
+
