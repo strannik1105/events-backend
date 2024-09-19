07c31168c7d28f086c7d0895cdb76822bb169f4f
strannik1105 <danil.stolpovskikh@gmail.com>
2024-09-19 15:38:58 +0000

    fix: uvicorn run with host and port from Config

--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
-import uvicorn
-
-from app.app import ApiRegistrator, AppMaker
+from app.app import ApiRegistrator, App
 from events.api.event import EventApi
 
 
 def main() -> None:
-    app = AppMaker().get_instance()()
+    app = App().get_instance()
 
     ApiRegistrator.register(app, EventApi.get_instance(), "event")
 
-    uvicorn.run(app)
+    app.run()
 
 
 if __name__ == "__main__":
