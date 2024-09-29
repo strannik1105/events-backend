b2dc95a4b8ea3c2da5b978a2ab8b9b701550cc25
strannik1105 <danil.stolpovskikh@gmail.com>
2024-09-29 10:06:43 +0000

    feat: add route to publish messages into telegram

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from app.app import ApiRegistrator, App
 from events.api.event import EventApi
 from events.api.event_type import EventTypeApi
+from utils.tg_api import TgApi
 
 
 def main() -> None:
@@ -8,6 +9,7 @@ def main() -> None:
 
     ApiRegistrator.register(app, EventApi.get_instance(), "event")
     ApiRegistrator.register(app, EventTypeApi.get_instance(), "event_type")
+    ApiRegistrator.register(app, TgApi.get_instance(), "telegram")
 
     app.run()
 
